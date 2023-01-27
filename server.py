ae894b003a1d88eaaca620d7445cfcc0a9b39b40
Jingyi Xu <jx15@ualberta.ca>
2023-01-27 04:53:29 -0700

    version 2

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
 import os
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,15 +40,15 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
         # # extract information: request_type, path
         # request_type = self.data[0].split()[0]  # GET
-        # path = self.data[0].split()[1] 
+        # path = self.data[0].split()[1]
         # protocol = self.data[0].split()[2]
         # ==========================================================================
         # USER STORY: As a webserver admin I want ONLY files in ""./www" and deeper to be served
         expected_path = './www' + path
         # requested_path = os.path.abspath(path)
-        
-        file_type = "" 
-        if (".html" in path):  
+
+        file_type = ""
+        if (".html" in path):
             file_type = "text/html"
         elif (".css" in path):
             file_type = "text/css"
@@ -56,7 +56,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
         # USER STORY: As a webserver admin I want to serve HTML and CSS files from ./www
         # determine the file type first to process the request separetely
         # https://www.geeksforgeeks.org/python-os-path-commonprefix-method/
-        
+
         file_relative_path = self.get_file_relative_path(path, expected_path)
 
         if (file_relative_path is None):
@@ -72,28 +72,28 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
         elif (file_type == ""):
             # directory
-            
+
             if file_relative_path.endswith('/'):
                 print("this for 301 with /")
                 response = self.get_content_dir(file_relative_path)
                 self.request.sendall(bytearray(response, "utf-8"))
-                
-            else:    
+
+            else:
                 # 301 moved
                 print("this is test for without / endwith")
                 print("file for the file_relative_path %s", file_relative_path)
-                # ./wwww/deep  -> /deep 
-                response = self.moved_path(path) 
-                self.request.sendall(bytearray(response, "utf-8"))   
-                return         
+                # ./wwww/deep  -> /deep
+                response = self.moved_path(path)
+                self.request.sendall(bytearray(response, "utf-8"))
+                return
 
         # elif (os.path.commonprefix([expected_path,file_relative_path]) != expected_path):
-        
+
         elif (file_type in ["text/html","text/css"]):
             # 404 errors for paths not found
-            # check if file exist 
+            # check if file exist
             print("xxxxxxx: the file_re_path %s", file_relative_path)
-            
+
             if ((os.path.exists(file_relative_path)) and (not self.directory_checker(file_relative_path))):
                 # 200
                 response = self.get_content(file_relative_path,file_type)
@@ -107,7 +107,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
             # 200
             response = self.get_content(file_relative_path,file_type)
             self.request.sendall(bytearray(response, "utf-8"))
-                
+
             # try:
             #     # it will detect if the file path is inside this directory
             #     # if not, will be IsADirectoryError and status code will be 301
@@ -117,13 +117,13 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
             # except IsADirectoryError:
             #     # do we need to test if file_relative_path.endwith("/")
-            #     # Must use 301 to correct paths such as http://127.0.0.1:8080/deep 
+            #     # Must use 301 to correct paths such as http://127.0.0.1:8080/deep
             #     # to http://127.0.0.1:8080/deep/  ./www/deep  /deep.css
 
             #     response = self.moved_path(str(file_relative_path) +"/")
             #     self.request.sendall(bytearray(response, "utf-8"))
 
-            # # everything correct 
+            # # everything correct
             # self.request.sendall(bytearray(response,'utf-8'))
 
     def directory_checker(self, file_relative_path):
@@ -151,7 +151,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
         print("this is for test 301 response %s", response_301)
         return response_301
 
-        
+
     def wrong_request(self):
         status_code = 405
         content = ""
@@ -174,9 +174,9 @@ class MyWebServer(socketserver.BaseRequestHandler):
             response = "HTTP/1.1 %i Method Not Allowed\r\n" %status
         elif status == 301:
             response = "HTTP/1.1 %i Moved Permanently\r\n" %status
-            response += "Location: http://localhost:8080%s/ \r\n" %path  
+            response += "Location: %s/\r\n" %path  
         return response
-           
+
     def get_content(self, expected_path,file_type):
 
         f = open(expected_path, 'r')
@@ -186,17 +186,19 @@ class MyWebServer(socketserver.BaseRequestHandler):
         "Connection: Closed\r\n" + content
         f.close()
         return header
-        
-        
+
+
     def get_file_relative_path(self, path, expected_path):
         # for if and elif -> check if path is with dir ./www
         path = "./www" +path
-        if os.path.isfile(expected_path):
-            print("xxxx: find the file")
+        if (os.path.exists(path)):
             return path
-        elif os.path.isdir(expected_path):
-            # return os.path.join(path, "index.html")
-            return path
+        # if os.path.isfile(expected_path):
+        #     print("xxxx: find the file")
+        #     return path
+        # elif os.path.isdir(expected_path):
+        #     # return os.path.join(path, "index.html")
+        #     return path
         else:
             response = self.wrong_path()
             self.request.sendall(bytearray(response,'utf-8'))
@@ -204,7 +206,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
 
 
-        
+
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
