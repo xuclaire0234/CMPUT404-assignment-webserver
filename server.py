f96ee49f783a65c53ee2ff61a9966e7f26d3ce01
Jingyi Xu <jx15@ualberta.ca>
2023-01-27 05:03:51 -0700

    update license

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #  coding: utf-8
 import socketserver
 import os
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Jingyi Xu
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
